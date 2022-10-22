5a568ffb73ae8e2d66e3b03812b6236e15e3f63a
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2022-10-22 15:07:33 +0900

    페이지 탐색처리 추가, 뒤로가기 버튼 클릭 시 이전 웹페이지로 이동

--- app/src/main/java/com/hyeonwoo/android_webview_sample/MainActivity.kt
+++ app/src/main/java/com/hyeonwoo/android_webview_sample/MainActivity.kt
@@ -2,15 +2,29 @@ package com.hyeonwoo.android_webview_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+    lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView = findViewById<WebView>(R.id.webview)
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
         webView.loadUrl("https://www.daangn.com/")
 
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check if the key event was the Back button and if there's history
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
     }
 }
